4cc01242db36e192c102f30d3126599fac2c70c6
ali-lasharivj <queensapna26@gmail.com>
2025-09-29 11:13:26 +0500

    Update tools.js

--- plugins/tools.js
+++ plugins/tools.js
@@ -20,6 +20,11 @@ const { gmd, config, commands, GiftedFancy, Giftedttstalk, giftedTempmail, gifte
 
 function formatBytes(bytes) {
   if (bytes === 0) return '0 Bytes';
+  const k = 1024;
+  const sizes = ['Bytes', 'KB', 'MB', 'GB'];
+  const i = Math.floor(Math.log(bytes) / Math.log(k));
+  return parseFloat((bytes / Math.pow(k, i)).toFixed(2)) + ' ' + sizes[i];
+}
 
 
 gmd({
